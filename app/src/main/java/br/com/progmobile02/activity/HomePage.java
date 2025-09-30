7df70ade88aacbab77a6bf7ce4d6d44ca030c924
rafinho0 <rafa06osilva@gmail.com>
2025-09-30 18:17:49 -0300

    Finalized the counter logic

--- app/src/main/java/br/com/progmobile02/activity/HomePage.java
+++ app/src/main/java/br/com/progmobile02/activity/HomePage.java
@@ -2,7 +2,10 @@ package br.com.progmobile02.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,23 +13,76 @@ import br.com.progmobile02.activity.databinding.ActivityHomePageBinding;
 
 public class HomePage extends AppCompatActivity {
 
-    private ActivityHomePageBinding binding;
+    private int counterInt = 0;
+    private TextView counterStr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
 
-        setContentView(binding.getRoot());
+        // Dessa vez nao utilizei o View Binding pois nao consegui fazer dessa forma
+        setContentView(R.layout.activity_home_page);
 
-        binding.tvSignOut.setOnClickListener(new View.OnClickListener() {
+
+        // Referenciamos o botao de sair
+        TextView tvSignOut = findViewById(R.id.tvSignOut);
+
+        // Cada atributo referenciando o id do elemento
+        counterStr = findViewById(R.id.counter);
+        Button add = findViewById(R.id.add);
+        Button reset = findViewById(R.id.reset);
+        Button subtract = findViewById(R.id.subtract);
+
+        // Funcao para atualizar o contador
+        updateCounter();
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterInt++;
+                updateCounter();
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterInt = 0;
+                updateCounter();
+            }
+        });
+
+
+        // Deixei a opção do número poder ser negativo
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterInt--;
+                updateCounter();
+            }
+        });
+
+
+        tvSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Log.d("HomePage", "Saindo via findViewById.");
+
+                // Aqui iremos limpar a pilha da memoria
                 Intent intent = new Intent(HomePage.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
                 startActivity(intent);
+                finish();
             }
         });
+    }
+
+    private void updateCounter() {
+
+        counterStr.setText(String.valueOf(counterInt));
     }
 }
